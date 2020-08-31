fa8f872b6579bfecc82f971fc2a70cd49b20c1e4
Carlos Abia <carlosabiamer@gmail.com>
2020-08-31 17:32:43 +0200

    Update niveles

--- js/verbos.js
+++ js/verbos.js
@@ -373,7 +373,7 @@ function generaTabla(array, level) {
         if (level === 'easy') {
             objetivoVerbos = 2;
         } else if (level === 'medium') {
-            objetivoVerbos = 100;
+            objetivoVerbos = 120;
         } else objetivoVerbos = 146;
     } else {
         array = verbos;
